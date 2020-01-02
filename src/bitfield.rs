b8bcebfc18ab2ea0e29cc5e4779292651baaf8f8
David Kern <david@mju.io>
2020-01-01 17:13:53 -0800

    Additional documentation

--- src/bitfield.rs
+++ src/bitfield.rs
@@ -31,18 +31,35 @@ use typenum::{
     Xor,
 };
 
+/// Marker type representing an owned bitfield.  `TOwner` is the owning type of the
+/// bitfield.  `Shift` and `Mask` are `typenum::Unsigned` types representing the shift and mask
+/// values used to access the bitfield.  These are both converted to constants in the `BitFieldTrait`
+/// implemented for this struct.
+///
+/// While this type can be used directly, it is more convenient to use the `BitField` type
+/// alias to define a bit range rather than the lower-level shift and mask.
 pub struct BitFieldImpl<TOwner, Shift, Mask>(
     PhantomData<TOwner>,
     PhantomData<Shift>,
     PhantomData<Mask>,
 );
 
+/// Tracks the `Owner`, shift and mask used by the `Storage::*` methods to provide access
+/// to an owned bit field.
 pub trait BitFieldTrait {
+    /// The owner, typically a `Storage`, bound to this bit field
     type Owner;
+
+    /// The number of bits the field is shifted in the owner
     const SHIFT: u32;
+
+    /// The bit mask used to to select or clear the field bits in the owner
     const MASK: u64;
 }
 
+/// Implementation for `BitFieldImpl` explicitly converts the typenum shift and mask calculations
+/// into constants. Forcing the conversion here prevents infinite recursion by the compiler when
+/// these values are used by `Storage` to provide access to the field.
 impl<TOwner, TShift, TMask> BitFieldTrait for BitFieldImpl<TOwner, TShift, TMask>
 where
     TShift: Unsigned,
@@ -56,7 +73,10 @@ where
 type BitMask<X> = Shleft<U1, X>;
 type RightMask<X> = Sub1<BitMask<X>>;
 
-/// Define a BitField including bits from `X` to `Y`, inclusively, for `TOwner`
+/// Define a BitField bound to `TOWner` (typically a `Storage`).  The bit range of
+/// the field includes bits from `X` to `Y` inclusively.  The ordering of bit positions
+/// does not matter: `BitField<Owner, B0, B7>` defines the same field as `BitField<Owner, B7, B0>`
+/// although technically the two are distinct types.
 pub type BitField<TOwner, X, Y> = BitFieldImpl<
     TOwner,
 
