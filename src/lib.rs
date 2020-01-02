b61348306eb090f48217535c4e97d4de970291d8
David Kern <david@mju.io>
2020-01-01 17:52:52 -0800

    Update to version 0.2.0

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-#![doc(html_root_url = "https://docs.rs/mju-bits/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/mju-bits/0.2.0")]
 
 //! `#[no_std]` implementation of typed bitfield access for `u8`, `u16`, `u32`, `u64` and `usize`.
 //!
